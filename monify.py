20cc8deefdefcbe148a8262dfe507df68eb44d43
peterdtitan <p.okorafor@alustudent.com>
2021-12-09 14:32:19 +0200

    Project changes

--- monify.py
+++ monify.py
@@ -1,7 +1,10 @@
 import datetime
 from pandas import DataFrame as df
 import pandas as pd
-from matplotlib import pyplot,style
+from csv import writer
+import glob
+import os.path
+import os
 
 
 def checkIf(month):
@@ -30,7 +33,7 @@ def check_month(month):
     return switch.get(month,1)
 
 
-def tracker(date = datetime.date.today() ,income =" ", foodDrinks =" ", Transport =" ", lifeEntertainment =" ", miscellaneous =" ", housing= " ", total_expenses =" "):
+def tracker(date = 0 ,income =" ", foodDrinks =" ", Transport =" ", lifeEntertainment =" ", miscellaneous =" ", housing= " ", total_expenses =" "):
     data = {
     'Date':date,
     'Income':income,
@@ -55,26 +58,15 @@ def tracker(date = datetime.date.today() ,income =" ", foodDrinks =" ", Transpor
     database.to_csv(f'{curr_month}.csv',mode = decide,header=check,index=False)
 
 class Monify:
-  def __init__ (self, monthly = 0.0, food_drinks = 0.0 , transport = 0.0, housing = 0.0, life_entertainment = 0.0, miscellaneous = 0.0, emergency_fund = 0.0, available_income =" " ):
-    self.available_income = available_income
-    self.budget_monthly =   monthly
-    self.budget_food_drinks = food_drinks
-    self.budget_transport = transport
-    self.budget_housing = housing
-    self.budget_life_entertainment = life_entertainment
-    self.budget_miscellaneous = miscellaneous
-    self.budget_emergency_fund = emergency_fund
-
-
-  def add_income(self):
-    date = str(input('Enter Date (YYYY-MM-DD) (optional) \n: --> '))
+  def add_income():
+    date = str(input('Enter Date (YYYY-MM-DD)\n: --> '))
     year,month,day=map(int,date.split('-'))
     date = datetime.date(year,month,day)
     income = int(input("Enter your income for the month \n: --> "))
     tracker(date,income,0,0,0,0,0,0)
     
 
-  def add_expenditure(self):
+  def add_expenditure():
     print("\t\t\t\nEXPENSE CATEGORIES: \n1. Food & Drinks\n2. Transport\n 3. Life & Entertainment\n4. Miscellaneous\n5. Housing\n")
     
     while True:
@@ -94,15 +86,17 @@ class Monify:
                 year,month,day=map(int,date.split('-'))
                 date = datetime.date(year,month,day)
                 
-                fd = str(input("\nHow much did you spend on \"Food and Drinks?\""))
-                print("\n")
+                fd = str(input("\nHow much did you spend on \"Food and Drinks?\" --> "))
+                
             except ValueError:
                 print("Please enter valid input!\n")
                 continue
+              
             else:
                 tracker(date,0,fd,0,0,0,0,0)
                 print("\nExpense recorded successfully!\n")
                 break
+            
             
     elif response == 2:
         while True:
@@ -111,15 +105,17 @@ class Monify:
                 year,month,day=map(int,date.split('-'))
                 date = datetime.date(year,month,day)
                 
-                ht = str(input("\nHow much did you spend on \"Transport?\""))
-                print("\n")
+                ht = str(input("\nHow much did you spend on \"Transport?\" --> "))
+
             except ValueError:
                 print("Please enter valid input!\n")
                 continue
+              
             else:
                 tracker(date,0,0,ht,0,0,0,0)
                 print("\nExpense recorded successfully!\n")
                 break
+
 
     elif response == 3:
         while True:
@@ -128,15 +124,17 @@ class Monify:
                 year,month,day=map(int,date.split('-'))
                 date = datetime.date(year,month,day)
                 
-                le = str(input("\nHow much did you spend on \"Life & Entertainment?\""))
-                print("\n")
+                le = str(input("\nHow much did you spend on \"Life & Entertainment?\" --> "))
+
             except ValueError:
                 print("Please enter valid input!\n")
                 continue
+              
             else:
                 tracker(date,0,0,0,le,0,0,0)
                 print("\nExpense recorded successfully!\n")
                 break
+
 
     elif response == 4:
         while True:
@@ -145,8 +143,8 @@ class Monify:
                 year,month,day=map(int,date.split('-'))
                 date = datetime.date(year,month,day)
                 
-                misc = str(input("\nHow much did you spend on \"Miscellaneous?\""))
-                print("\n")
+                misc = str(input("\nHow much did you spend on \"Miscellaneous?\" --> "))
+                
             except ValueError:
                 print("Please enter valid input!\n")
                 continue
@@ -155,6 +153,7 @@ class Monify:
                 print("\nExpense recorded successfully!\n")
                 break
 
+
     elif response == 5:
         while True:
             try:
@@ -162,18 +161,23 @@ class Monify:
                 year,month,day=map(int,date.split('-'))
                 date = datetime.date(year,month,day)
                 
-                house = str(input("\nHow much did you spend on \"Housing?\""))
-                print("\n")
+                house = str(input("\nHow much did you spend on \"Housing?\" --> "))
+        
             except ValueError:
                 print("Please enter valid input!\n")
                 continue
+              
             else:
                 tracker(date,0,0,0,0,0,house,0)
                 print("\nExpense recorded successfully!\n")
                 break
+    
             
-  def add_budget(self):
-    print(f"You have {self.available_income} and cannot budget than it")
+  def add_budget():
+    
+    
+    print("\nYou are about to enter your budget for the CURRENT MONTH")
+    
     while True:
       try:
         user_option = str(input("\n Would you like to proceed (yes/no): "))
@@ -183,89 +187,130 @@ class Monify:
         continue
       else:
         break
-    if user_option == "YES":
-      monthly_budget = float(input("Enter your monthly_budget\n: --> "))
-      self.budget_monthly += monthly_budget
+      
+    
 
-      food_drinks_budget = float(input("Enter your budget for food_drink\n: --> "))
-      self.budget_food_drinks += food_drinks_budget
 
-      transport_budget = float(input("Enter your budget for transport\n: --> "))
-      self.budget_transport += transport_budget
+  def emergency_fund():
+    emergency_fund_amount = int(input("How much do you want to add to your Emergency fund account: "))
+    
+  
+  
+  def view_shopping_list():
+    if os.path.exists('./Shopping List.csv'):
+      list = pd.read_csv('Shopping List.csv', header=0)
+      print("\nThis is your most recent shopping list: --> \n")
+      print(list)
+    else:
+      print("\nYou have not added any shopping list yet!")
+    
 
-      life_entertainment_budget = float(input("Enter your budget for life_entertainment\n: --> "))
-      self.budget_life_entertainment += life_entertainment_budget
-
-      misc_budget = float(input("Enter your budget for miscellaneous\n: --> "))
-      self.budget_miscellaneous += misc_budget
-
-      house_budget = float(input("Enter your budget for housing\n: --> "))
-      self.budget_housing += house_budget
-
-    else: 
-      self.menu()
-
-  def emergency_fund(self):
-    emergency_fund_amount = float(input("How much do you want to save in your Emergency fund account\n:--> "))
-    while True:
-      try: 
-        if self.available_income  and emergency_fund_amount < self.available_income:
-          self.budget_emergency_fund += emergency_fund_amount
-          print(f"You saved {self.budget_emergency_fund} in your Emergency_fund account")
-        else:
-          print("Insufficient balance")
-      except ValueError:
-        print("Please enter valid input!\n")
+  def add_shopping_list():
+    shopping_list = {}
+    ans = True
+    while ans:
+      print("\nEnter the items in your shopping list and their prices -->")
+      item = str(input("\nEnter the item name: --> "))
+      value = int(input("\nEnter the price: --> "))
+      shopping_list[item] = value
+      
+      print("Recorded!\n")
+      resp = str(input("Add another item? (yes/no) --> "))
+      resp = resp.upper()
+      if resp == "YES":
         continue
       else:
-        break 
-
-  def shopping_list(self):
-    shopping_details = input("Enter the details for your shopping list, separating each entry by a comma.\n:--> ")
-    list_format = list(map(str, shopping_details.split(",")))
-    for item in list_format:
-      print("Here is your shopping list details:\n{item}")
-
-
-
-class Debt_account(Monify):
-  def __init__(self,available_income, debt_dictionary = {},):
-    super().__init__(available_income)
-    self.debt_dictionary = debt_dictionary
-
-  def add_debt_record(self):
-    borrowee = input("Enter the Borrowee who you owe\n: --> ")
-    loan_amount = float(input("How much did you borrow from this user\n: --> "))
-    self.debt_dictionary[borrowee] = loan_amount
-
-  def pay_debt(self):
-    print(f"Your debt account: {self.debt_dictionary}")
-    user_choice_pay = input("What debt would you like to pay\n : --> ")
-    if user_choice_pay in self.debt_dictionary:
-      self.available_income -= self.debt_dictionary["user_choice_pay"]
-      del self.debt_dictionary["user_choice_pay"]
-
-
-
-
-
-
-
-
-
-
+        ans = False
+        df = pd.DataFrame.from_dict(shopping_list, orient = 'index')
+        df = df.transpose()
+        
+        if os.path.exists('./Shopping List.csv'):
+          os.remove('Shopping List.csv')
+          
+        df.to_csv('Shopping List.csv')
+        
 
         
 
+ 
+
+def debt_data(date = 0, lender = " ", amount = " ", due = " ",):
+  data = {
+  'Date':date,
+  'Lender':lender,
+  'Amount Borrowed':amount,
+  'Repayment Date': due
+  }
+
+  if os.path.exists('./Debt Records.csv'):
+    list = [date,lender,amount,due]
+    with open('Debt Records.csv', 'a') as f_object:
+      writer_object = writer(f_object)
+      writer_object.writerow(list)
+      f_object.close() 
+  else:
+    df = pd.DataFrame.from_dict(data, orient = 'index')
+    df = df.transpose()
+    df.to_csv('Debt Records.csv', index = False)
 
 
+class Debt_account(Monify):
+  def add_debt_record():
+    amount = input("\nHow much did you borrow? --> ")
+    date = input("\nWhen did borrow this money? -->  ")
+    lender = input("\nEnter the name of the lender who you owe: --> ")
+    due = input("\nWhat date do you need to pay back?: --> ")
+    
+    debt_data(date, lender, amount, due)
+
+  def pay_debt():
+    data = pd.read_csv('Debt Records.csv', header=0)
+    print(data)
+    
+    response = int(input("\nWhich loan have you payed back? (Enter row number) --> "))
+    data = data.drop(index = response)
+  
+    if os.path.exists('./Debt Records.csv'):
+      os.remove('Debt Records.csv')
+      data.to_csv('Debt Records.csv')
+    
 
 
-
-
-
-
-c1 = Monify()
-c1.add_income()
-
-
+def menu():
+  print("\n\t\t########################### WELCOME TO MONIFY ###########################")
+  print("\n\t\tWhat do you want to do?\n\t\t1. Add Income\n\t\t2. Add Expenditure\n\t\t3. Debt Accounting\n\t\t4. Shopping List\n\t\t5. Emergency Funds\n")
+  
+  while True:
+    try:
+      ans = int(input("Please enter an option: --> "))
+    except ValueError:
+      print("\nPlease enter an integer!")
+      print("\n\t\t########################### WELCOME TO MONIFY ###########################")
+      print("\n\t\tWhat do you want to do?\n\t\t1. Add Income\n\t\t2. Add Expenditure\n\t\t3. Debt Accounting\n\t\t4. Shopping List\n\t\t5. Emergency Funds\n6. Statistics")
+      continue
+    else:
+      break
+    
+  if ans == 1:
+    Monify.add_income()
+    
+  elif ans == 2:
+    Monify.add_expenditure()
+    
+  elif ans == 3:
+    while True:
+      try:
+        print("\n\t\t1. Add Debt Record\n\t\t2. Pay Debt\n")
+        debt = int(input("What do you want to do? --> "))
+      except ValueError:
+        print("\nPlease enter an integer!\n")
+        continue
+      else:
+        break
+    if debt == 1:
+      Debt_account.add_debt_record()
+    elif debt == 2:
+      Debt_account.pay_debt()
+      
+Monify.add_shopping_list()
+Monify.view_shopping_list()
